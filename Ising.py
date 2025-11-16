809743f1329976dec37b71d3b21c5e6bc5a28f3f
Carter <cmatties2003@gmail.com>
2025-11-15 20:39:41 -0500

    Compute energy of a given grid

---
+++ Ising.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+
+def Ising_energy(grid, J, h):
+    left_shift = shift_left(grid)
+    right_shift = shift_right(grid)
+    up_shift = shift_up(grid)
+    down_shift = shift_down(grid)
+    
+    interactions = 0.5*np.sum(left_shift * grid + right_shift * grid + up_shift * grid + down_shift * grid)
+    background = np.sum(grid)
+    total_energy = -J*interaction - h*grid
+    return total_energy
+
+
+def shift_down(grid):
+    N = grid.shape[0]
+    last_row = np.reshape(grid[-1, :], (1, N))
+    return np.append(last_row, grid[:-1, :], axis=0)
+
+
+def shift_up(grid):
+    N = grid.shape[0]
+    first_row = np.reshape(grid[0, :], (1, N))
+    return np.append(grid[1:, :], first_row, axis=0)
+
+
+def shift_left(grid):
+    N = grid.shape[0]
+    first_col = np.reshape(grid[:, 0], (N, 1))
+    return np.append(grid[:, 1:], first_col, axis=1)
+
+
+def shift_right(grid):
+    N = grid.shape[0]
+    last_col = np.reshape(grid[:, -1], (N, 1))
+    return np.append(last_col, grid[:, :-1], axis=1)
